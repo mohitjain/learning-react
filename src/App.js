94c949fa21ee88a2e93b00039d85d469479a7b7c
Mohit Jain <jain.mohit27@gmail.com>
2020-08-09 07:14:53 +0530

    Event to change a state

--- src/App.js
+++ src/App.js
@@ -16,11 +16,30 @@ class App extends Component {
             }
         ]
     }
+
+    switchNameHandler = () => {
+        // Don't use this. Changing the state directly
+        // this.state.persons[0].name = "Mohit Jain"
+
+        this.setState({ persons: [
+                {
+                    name: "Mohit Jain", age: 32
+                },
+                {
+                    name: "Sakshi", age: 27
+                },
+                {
+                    name: "Nipun", age: 29
+                }
+            ] })
+    }
+
     render() {
         return (
             <div className="App">
                 <h1>Hi I am a React App.</h1>
                 <p>This is working.</p>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
                 <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age }/>
                 <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }> My hobbies: Running</Person>
                 <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age }/>
