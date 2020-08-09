97b2fb04cbb1e39b9704ed66d89a28879d20bd85
Mohit Jain <jain.mohit27@gmail.com>
2020-08-09 07:05:42 +0530

    Reading data from state object

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+    state = {
+        persons: [
+            {
+                name: "Mohit", age: 32
+            },
+            {
+                name: "Sakshi", age: 27
+            },
+            {
+                name: "Nipun", age: 28
+            }
+        ]
+    }
     render() {
         return (
             <div className="App">
                 <h1>Hi I am a React App.</h1>
                 <p>This is working.</p>
-                <Person name="Mohit" age='32'/>
-                <Person name="Nipun" age='27'> My hobbies: Running</Person>
-                <Person name="Sakshi" age='26'/>
+                <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age }/>
+                <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }> My hobbies: Running</Person>
+                <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age }/>
             </div>
         );
         // return React.createElement('div', { className: 'App' }, 'I am a react app');
