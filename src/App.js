f9760d9187946485d76de8fe00bc5b479f0c6111
Mohit Jain <jain.mohit27@gmail.com>
2020-08-11 03:48:21 +0530

    Basic CSS dynamic assignment

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
 
     render() {
         const style = {
-            backgroundColor: 'white',
+            backgroundColor: 'green',
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
@@ -75,11 +75,23 @@ class App extends Component {
                     })}
                 </div>
             )
+            style.backgroundColor = "Red";
         }
+
+        const classes = [];
+
+        if(this.state.persons.length <= 2){
+            classes.push('red');
+        }
+
+        if(this.state.persons.length <= 1){
+            classes.push('bold');
+        }
+
         return (
             <div className="App">
                 <h1>Hi I am a React App.</h1>
-                <p>This is working.</p>
+                <p className={classes.join(" ")}>This is really working.</p>
                 <button style={style} onClick={this.togglePersonHandler}>Toggle Persons</button>
                 {persons}
             </div>
